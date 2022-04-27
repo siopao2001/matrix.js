546e13be99539cce2ab6550779fa94b2d25fa0ff
siopao2001 <sio_pao2001@yahoo.ca>
2022-04-27 14:30:00 +0000

    Added sum function

--- matrix.js
+++ matrix.js
@@ -1,2 +1,8 @@
+let arg = process.argv.slice(2);
 
-let test = 'test'
+let sum = (num1, num2) => {
+    let total = Number(num1) + Number(num2);
+    return total;
+};
+
+console.log(sum(arg[0], arg[1]));
